561558841e5a8baa873f84e18fd9b468e37bd584
jia2337610467 <45751622+jia2337610467@users.noreply.github.com>
2021-02-03 16:19:00 +0800

    提交导航栏

---
+++ src/components/Header/index.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="header">
+    <div class="header-portrait">
+      <img src="@/assets/portrait.svg" alt="LOGO" />
+    </div>
+    <div class="header-list">
+      <router-link to="">首页</router-link>
+      <router-link to="">文章</router-link>
+      <router-link to="">笔记</router-link>
+      <router-link to="">关于</router-link>
+    </div>
+    <div class="header-search">
+      <i class="icon-sousuo"></i>
+      <input
+        type="text"
+        aria-label="搜索"
+        autocomplete="off"
+        spellcheck="false"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import { reactive, toRefs } from "vue";
+export default {
+  name: "Header",
+  setup() {
+    const data = reactive({
+      head: ""
+    });
+
+    return { ...toRefs(data) };
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.header {
+  width: 100%;
+  height: 70px;
+  background: #fff;
+  border-bottom: 1px solid rgb(211, 211, 211);
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  &-portrait {
+    img {
+      width: 60px;
+      height: 65px;
+    }
+  }
+  &-list {
+    width: 450px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    a {
+      font-size: 20px;
+      font-weight: 600;
+      font-family: "PingFang SC";
+      &::after {
+        content: "";
+      }
+    }
+  }
+  &-search {
+  }
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import "./registerServiceWorker";
+import "./style/index.scss";
+import "./style/font.scss";
 import router from "./router";
 import store from "./store";
 
