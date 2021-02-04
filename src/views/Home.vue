38582cea33ef07fa9faf9a19b8f56a0102ef63eb
jia2337610467 <45751622+jia2337610467@users.noreply.github.com>
2021-02-04 17:00:40 +0800

    提交代码

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,41 @@
 <template>
   <div class="home">
     <Header />
+    {{ agent }}
   </div>
 </template>
 
 <script>
 import Header from "@/components/Header";
-
+import { onMounted, ref } from "vue";
 export default {
   name: "Home",
   components: {
     Header
+  },
+  setup() {
+    const agent = ref("");
+
+    const order = () => {
+      let ua = window.navigator.userAgent.toLowerCase();
+      if (ua.match(/MicroMessenger/i) == "micromessenger") {
+        console.log("微信端");
+        agent.value = 1;
+      } else if (ua.match(/Alipay/i) == "alipay") {
+        console.log("支付宝端");
+        agent.value = 2;
+      } else {
+        console.log("其他浏览器");
+        agent.value = 3;
+      }
+    };
+    onMounted(() => {
+      order();
+    });
+
+    return {
+      agent
+    };
   }
 };
 </script>
