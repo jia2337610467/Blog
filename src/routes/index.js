fe3ecdcc0f5b2ce9820991d1c9947643ca80f2c5
jiabingyue <2337610467@qq.com>
2021-04-11 22:00:26 +0800

    ...

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,6 +5,11 @@ const router = createRouter({
     // h5模式 createWebHistory(),
   history: createWebHashHistory(),
   routes: routing,
+
+  scrollBehavior(to,from,savePosition){
+    if(savePosition)return savePosition;
+    else return {left:0,top:0}
+  }
 });
 
 export default router;
